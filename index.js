b9f420164d62bbf261bd21d36c167b012030c3e8
Sergio Perez <sergio13prez@gmail.com>
2019-07-23 02:19:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item) {
   // write your code here
   for(var i = 0; i < cart.length; i++){
     if(cart[i].itemName === item){
-      return cart
+      return cart.splice()
     }
   }
 }
