3cebf94be5a0db306fce5dad280f50eec2f8e8be
Sergio Perez <sergio13prez@gmail.com>
2019-07-23 01:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
   var itemString ='';
   
   if(cart.length === 1){
-    itemString = `you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+    itemString = `you have ${cart[0].itemName} at $${cart[0].itemPrice}`;
   } else if (cart.length === 2){
     itemString = 
   }
